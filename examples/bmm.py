Correct units for time printouts (#133)
do_bench returns ms rather than sec

examples/bmm.py
@@ -35,7 +35,7 @@ def check(b: int, m: int, k: int, n: int) -> None:
     sec = do_bench(lambda: bmm(x, y))
     baseline_sec = do_bench(lambda: torch.bmm(x, y))
     print(
-        f"Helion time: {sec:.4f}s, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
+        f"Helion time: {sec:.4f}ms, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
     )
 
 

examples/concatenate.py
@@ -41,7 +41,7 @@ def main() -> None:
     sec = do_bench(lambda: concat2d_dim1(x, y))
     baseline_sec = do_bench(lambda: torch.cat([x, y], dim=1))
     print(
-        f"Helion time: {sec:.4f}s, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
+        f"Helion time: {sec:.4f}ms, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
     )
 
 

examples/embedding.py
@@ -34,7 +34,7 @@ def main() -> None:
     sec = do_bench(lambda: embedding(x, weight))
     baseline_sec = do_bench(lambda: torch.nn.functional.embedding(x, weight))
     print(
-        f"Helion time: {sec:.4f}s, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
+        f"Helion time: {sec:.4f}ms, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
     )
 
 

examples/softmax.py
@@ -64,7 +64,7 @@ def check(m: int, n: int) -> None:
     sec = do_bench(lambda: softmax(x))
     baseline_sec = do_bench(lambda: torch.nn.functional.softmax(x, dim=1))
     print(
-        f"Helion time: {sec:.4f}s, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
+        f"Helion time: {sec:.4f}ms, torch time: {baseline_sec:.4f}, speedup: {baseline_sec / sec:.2f}x"
     )
 
 